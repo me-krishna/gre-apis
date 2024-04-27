892d44a5131c278581851de3e76b213074a4e82a
Krishna <pulluru.krishnareddy@mileseducation.com>
2024-04-27 23:16:30 +0530

    cofigs

---
+++ utils/upload-config.ts
@@ -0,0 +1,14 @@
+import multer from "multer";
+import { v4 as uuidv4 } from "uuid";
+
+const date = new Date();
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, '/uploads/')
+  },
+  filename: function (req, file, cb) {
+    cb(null, uuidv4() + "-" + file.originalname)
+  }
+});
+
+export const upload = multer({ storage: storage });
