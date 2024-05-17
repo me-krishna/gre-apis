164c810b9bb16cd440288476a03f9c52ac746fd5
Krishna <pulluru.krishnareddy@mileseducation.com>
2024-05-17 11:48:30 +0530

    login routes updated

--- routes/auth.ts
+++ routes/auth.ts
@@ -4,5 +4,7 @@ import Login from "../controller/auth/login";
 const AuthRouter = Router();
 
 AuthRouter.post("/login", Login);
+AuthRouter.post("/student/login", Login);
+AuthRouter.post("/admin/login", Login);
 
 export default AuthRouter
